53b18d0e3bbac390fef94665796e72a04cee4629
Weidi Pan <panweidi2018@gmail.com>
2023-03-04 21:31:44 -0800

    Add a post Posts.

--- models/Post.js
+++ models/Post.js
@@ -1,117 +1,55 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const ProfileSchema = new Schema({
+const PostSchema = new Schema({
     user: {
         type: Schema.Types.ObjectId,
         ref: 'users'
     },
-    handle: {
-        type: String,
-        required: true,
-        max: 40
-    },
-    company: {
-        type: String
-    },
-    website: {
-        type: String
-    },
-    location: {
-        type: String
-    },
-    status: {
+    text: {
         type: String,
         required: true
     },
-    skills: {
-        type: [String],
-        required: true
-    },
-    bio: {
+    name: {
         type: String
     },
-    githubusername: {
+    avatar: {
         type: String
     },
-    experience: [
+    likes: [
         {
-            title: {
-                type: String,
-                required: true
-            },
-            company: {
-                type: String,
-                required: true
-            },
-            location: {
-                type: String
-            },
-            from: {
-                type: Date,
-                required: true
-            },
-            to: {
-                type: Date
-            },
-            current: {
-                type: Boolean,
-                default: false
-            },
-            description: {
-                type: String
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'users'
             }
         }
     ],
-    education: [
+    comments: [
         {
-            school: {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'users'
+            },
+            text: {
                 type: String,
                 required: true
             },
-            degree: {
-                type: String,
-                required: true
-            },
-            fieldofstudy: {
-                type: String,
-                required: true
-            },
-            from: {
-                type: Date,
-                required: true
-            },
-            to: {
-                type: Date
-            },
-            current: {
-                type: Boolean,
-                default: false
-            },
-            description: {
+            name: {
                 type: String
+            },
+            avatar: {
+                type: String
+            },
+            date: {
+                type: Date,
+                default: Date.now
             }
         }
     ],
-    social: {
-        youtube: {
-            type: String
-        },
-        twitter: {
-            type: String
-        },
-        facebook: {
-            type: String
-        },
-        linkedin: {
-            type: String
-        },
-        instagram: {
-            type: String
-        }
-    },
     date: {
         type: Date,
         default: Date.now
     }
 });
+
+module.exports = Post = mongoose.model('post', PostSchema);
