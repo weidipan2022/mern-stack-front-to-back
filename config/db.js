6795399da94f6a764f4eac3a6260a0227ba0debd
Weidi Pan <panweidi2018@gmail.com>
2023-03-04 19:01:25 -0800

    Develop a Users model and api

--- config/db.js
+++ config/db.js
@@ -4,7 +4,9 @@ const db = config.get('mongoURL');
 
 const connectDB = async () => {
     try {
+        // this is to avoid deprecation warnings
         mongoose.set('strictQuery', false)
+
         await mongoose.connect(db, {
             useNewUrlParser: true,
         });
